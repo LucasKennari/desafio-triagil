226c19521e68f04976e4fb997c2d66b173673bae
lucaskennari <lucas.oheisen@gmail.com>
2024-03-01 22:03:26 -0300

    adicionado um arquivo para simbolizar o banco de dados (banco de dados na memória)

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const team = require("./src/database/dbNaMemória/bancodedados");
 const app = express();
 const port = 3000;
 
 const route = require("express")();
+
+app.use(express.json());
+app.use(route);
 
 app.get("/:pokemon", async (req, res) => {
   const { pokemon } = req.params;
@@ -23,10 +27,36 @@ app.get("/:pokemon", async (req, res) => {
   res.json(pokemonPerfil);
 });
 
+app.get("/api/teams", () => {});
 app.get("/api/teams/:user", () => {});
 
-app.use(express.json());
-app.use(route);
+app.post("/api/teams", async (req, res) => {
+  const { owner, pokemons } = req.body;
+
+  const resultado = pokemons.map(async (pokemon) => {
+    const request = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    const data = await request.json();
+    let perfilPokemon = {
+      id: data.id,
+      name: data.name,
+      weight: data.weight,
+      height: data.height,
+    };
+    return perfilPokemon;
+  });
+
+  let perfilPokemon = await Promise.all(resultado);
+
+  team.push([
+    {
+      idTeam: 1,
+      user: owner,
+      team: perfilPokemon,
+    },
+  ]);
+
+  res.json(team);
+});
 
 app.listen(port, () => {
   console.log("Servidor on!!");
