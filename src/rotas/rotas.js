ebd43e64e38f060ffac73f66d407af34a3cc4db9
lucaskennari <lucas.oheisen@gmail.com>
2024-03-04 12:17:45 -0300

    mudando o nome das funcoes

--- src/rotas/rotas.js
+++ src/rotas/rotas.js
@@ -1,31 +1,16 @@
-const teams = require("../bancoDeDados/dbNaMemória/bancodedados");
-const buscarUmaEquipe = require("../controlador/buscarUmaEquipe/buscarUmaEquipe");
-const criarEquipe = require("../controlador/criarEquipe/criarEquipe");
-const listarTodasAsEquipes = require("../controlador/listarTodasAsEquipes/listarTodosOsTimes");
+const criarUmTime = require("../controlador/criarUmTime/criarUmTime");
+const buscarUmTime = require("../controlador/buscarUmTime/buscarUmTime");
+const listarTodosOsTimes = require("../controlador/listarTodosOsTimes/listarTodosOsTimes");
+
 const paginaNaoEncontrada = require("../controlador/paginaNaoEncontrada/paginaNaoEncontrada");
+
 const intermediario = require("../intermediario/intermediario");
 
 const rotas = require("express")();
 
-rotas.get("/:pokemon", async (req, res) => {
-  const { pokemon } = req.params;
-  const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
-  const obj = await data.json();
-
-  const { id, name, weight, height, ...resto } = obj;
-  const pokemonPerfil = {
-    id,
-    name,
-    weight,
-    height,
-  };
-
-  res.json(pokemonPerfil);
-});
-
-rotas.get("/api/teams", listarTodasAsEquipes);
-rotas.get("/api/teams/:user", buscarUmaEquipe);
+rotas.get("/api/teams", listarTodosOsTimes);
+rotas.get("/api/teams/:user", buscarUmTime);
 rotas.get("/api/*", paginaNaoEncontrada);
 
-rotas.post("/api/teams", intermediario, criarEquipe);
+rotas.post("/api/teams", intermediario, criarUmTime);
 module.exports = rotas;
