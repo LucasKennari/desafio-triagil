2bb1fa69e5b14d1cbe2df8978d596b9e78cf8465
lucaskennari <lucas.oheisen@gmail.com>
2024-03-04 12:18:39 -0300

    adicionado swagger

--- src/servidor/servidor.js
+++ src/servidor/servidor.js
@@ -1,9 +1,13 @@
 const rotas = require("../rotas/rotas");
+const swaggerUi = require("swagger-ui-express");
+
+const swaggerDocs = require("../swagger/swagger.json");
 
 const express = require("express");
 const servidor = express();
 
 const port = 3000;
+servidor.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 servidor.use(express.json());
 servidor.use(rotas);
